79bf65fd5ef074c61b3368078b5d5bb8d7f8cc2f
Jonas Matter <jonas.matter@bluewin.ch>
2018-04-20 15:04:52 +0200

    move from st_distance to <-> operator (performance improvement)

--- docker/db/function/105_create_pt_stop_way_function.sql
+++ docker/db/function/105_create_pt_stop_way_function.sql
@@ -1,9 +1,5 @@
 CREATE OR REPLACE FUNCTION min_distance(pt_stop GEOMETRY) RETURNS INTEGER AS $$
     SELECT routing.source FROM routing
-        ORDER BY ST_Distance(
-            ST_StartPoint(routing.geom_way),
-            ST_SetSRID(pt_stop, 4326),
-            true
-        ) ASC
+        ORDER BY ST_StartPoint(routing.geom_way) <-> ST_SetSRID(pt_stop, 4326)
         LIMIT 1
 $$ LANGUAGE sql;
