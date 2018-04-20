7f10d09c530f9fa2332f7ac92c9b5bf2e6ff1d6c
Jonas Matter <jonas.matter@bluewin.ch>
2018-04-20 16:29:17 +0200

    improve performance of pt_stop_way creation

--- docker/db/post-import-sql/osm-import/002_create_pt_stop_way_table.sql
+++ docker/db/post-import-sql/osm-import/002_create_pt_stop_way_table.sql
@@ -6,4 +6,4 @@ CREATE TABLE pt_stop_way (
     entry_way_id INTEGER NOT NULL REFERENCES routing(id)
 );
 
-INSERT INTO pt_stop_way (pt_stop_id, entry_way_id) SELECT pt_stop.id, min_distance(pt_stop.geom) from pt_stop;
+INSERT INTO pt_stop_way (pt_stop_id, entry_way_id) SELECT pt_stop.id, get_source(pt_stop.geom) from pt_stop;
