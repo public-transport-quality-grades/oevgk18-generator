f4b185acf738f4c446cd696d795fc554d83b458c
Robin Suter <suter.robin@gmail.com>
2018-04-23 10:16:17 +0200

    refactor calculate_transport_stop_rating

--- generator/business/transport_stop_rating_calculator.py
+++ generator/business/transport_stop_rating_calculator.py
@@ -1,8 +1,15 @@
 from .util.public_transport_group import PublicTransportGroup
 
 
-def calculate_transport_stop_rating(registry, uic_ref: str) -> PublicTransportGroup:
+def calculate_transport_stop_rating(registry, uic_ref: str):
     timetable_service = registry['timetable_service']
+    transport_group: PublicTransportGroup = calculate_transport_group(timetable_service, uic_ref)
+
+    # TODO calculate transport stop rating
+    return transport_group
+
+
+def calculate_transport_group(timetable_service, uic_ref: str) -> PublicTransportGroup:
 
     railway_direction_count = timetable_service.get_count_of_distinct_next_stops(uic_ref)
     if _is_railway_junction(railway_direction_count):
