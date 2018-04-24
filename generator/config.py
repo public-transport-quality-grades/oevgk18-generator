3f59bb9dd7d47e67b4b6b8bae0e0f2c4db50cb71
Robin Suter <suter.robin@gmail.com>
2018-04-24 13:20:01 +0200

    fix indendation in config module

--- generator/config.py
+++ generator/config.py
@@ -1,7 +1,7 @@
-import os
+from os import path
+from datetime import datetime
 import ruamel.yaml
 import jsonschema
-
 
 DEFAULT_CONFIG = """
 database-connections:
@@ -125,180 +125,180 @@ public-transport-ratings:
 """
 
 SCHEMA = {
-  "type": "object",
-  "definitions": {
-      "time-string": {
-          "type": "string",
-          "pattern": "^\d{2}:\d{2}$"
-      },
-      "transport-stop-interval": {
-          "type": "integer",
-          "minimum": 0
-      },
-      "pt-stop-category": {
-          "type": "integer",
-          "minimum": 1,
-          "maximum": 7
-      },
-      "pt-rating-category": {
-          "type": "string",
-          "pattern": "^[A-F]$"
-      }
-  },
-  "$schema": "http://json-schema.org/draft-07/schema#",
-  "properties": {
-    "database-connections": {
-      "$id": "/properties/database-connections",
-      "type": "object",
-      "properties": {
-        "public-transport-stops": {
-            "type": "string"
-        }
-      },
-      "additionalProperties": False,
-      "required": ["public-transport-stops"]
-    },
-    "due-dates": {
-      "$id": "/properties/due-dates",
-      "type": "array",
-      "items": {
-        "$id": "/properties/due-dates/items",
-        "type": "object",
-        "properties": {
-          "type-of-day": {
-            "$id": "/properties/due-dates/items/properties/type-of-day",
-            "type": "string"
-          },
-          "type-of-interval": {
-            "$id": "/properties/due-dates/items/properties/type-of-interval",
-            "type": "string"
-          },
-          "due-date": {
-            "$id": "/properties/due-dates/items/properties/due-date",
+    "type": "object",
+    "definitions": {
+        "time-string": {
             "type": "string",
-            "format": "date"
-          },
-          "lower-bound": {
-            "$id": "/properties/due-dates/items/properties/lower-bound",
-            "$ref": "#/definitions/time-string"
-          },
-          "upper-bound": {
-            "$id": "/properties/due-dates/items/properties/upper-bound",
-            "$ref": "#/definitions/time-string"
-          }
+            "pattern": "^\d{2}:\d{2}$"
         },
-        "additionalProperties": False,
-        "required": ["type-of-day", "type-of-interval", "due-date", "lower-bound", "upper-bound"]
-      }
-    },
-    "public-transport-types": {
-      "$id": "/properties/public-transport-types",
-      "type": "object",
-      "properties": {
-        "train-junction-min-directions": {
-          "$id": "/properties/public-transport-types/properties/train-junction-min-directions",
-          "type": "integer"
+        "transport-stop-interval": {
+            "type": "integer",
+            "minimum": 0
+        },
+        "pt-stop-category": {
+            "type": "integer",
+            "minimum": 1,
+            "maximum": 7
+        },
+        "pt-rating-category": {
+            "type": "string",
+            "pattern": "^[A-F]$"
         }
-      },
-      "additionalProperties": False,
-      "required": ["train-junction-min-directions"]
-
     },
-    "transport-stop-categories": {
-      "$id": "/properties/transport-stop-categories",
-      "type": "array",
-      "items": {
-        "$id": "/properties/transport-stop-categories/items",
-        "type": "object",
-        "properties": {
-          "max-interval": {
-            "$id": "/properties/transport-stop-categories/items/properties/max-interval",
-            "$ref": "#/definitions/transport-stop-interval"
-          },
-          "min-interval": {
-            "$id": "/properties/transport-stop-categories/items/properties/max-interval",
-            "$ref": "#/definitions/transport-stop-interval"
-          },
-          "transport-type-mappings": {
-            "$id": "/properties/transport-stop-categories/items/properties/transport-type-mappings",
+    "$schema": "http://json-schema.org/draft-07/schema#",
+    "properties": {
+        "database-connections": {
+            "$id": "/properties/database-connections",
+            "type": "object",
+            "properties": {
+                "public-transport-stops": {
+                    "type": "string"
+                }
+            },
+            "additionalProperties": False,
+            "required": ["public-transport-stops"]
+        },
+        "due-dates": {
+            "$id": "/properties/due-dates",
             "type": "array",
             "items": {
-              "$id": "/properties/transport-stop-categories/items/properties/transport-type-mappings/items",
-              "type": "object",
-              "properties": {
-                "A": {
-                  "$ref": "#/definitions/pt-stop-category"
+                "$id": "/properties/due-dates/items",
+                "type": "object",
+                "properties": {
+                    "type-of-day": {
+                        "$id": "/properties/due-dates/items/properties/type-of-day",
+                        "type": "string"
+                    },
+                    "type-of-interval": {
+                        "$id": "/properties/due-dates/items/properties/type-of-interval",
+                        "type": "string"
+                    },
+                    "due-date": {
+                        "$id": "/properties/due-dates/items/properties/due-date",
+                        "type": "string",
+                        "format": "date"
+                    },
+                    "lower-bound": {
+                        "$id": "/properties/due-dates/items/properties/lower-bound",
+                        "$ref": "#/definitions/time-string"
+                    },
+                    "upper-bound": {
+                        "$id": "/properties/due-dates/items/properties/upper-bound",
+                        "$ref": "#/definitions/time-string"
+                    }
                 },
-                "B": {
-                  "$ref": "#/definitions/pt-stop-category"
-                },
-                "C": {
-                  "$ref": "#/definitions/pt-stop-category"
-                }
-              },
-              "additionalProperties": False,
+                "additionalProperties": False,
+                "required": ["type-of-day", "type-of-interval", "due-date", "lower-bound", "upper-bound"]
             }
-          }
         },
-        "additionalProperties": False,
-        "required": ["transport-type-mappings"]
+        "public-transport-types": {
+            "$id": "/properties/public-transport-types",
+            "type": "object",
+            "properties": {
+                "train-junction-min-directions": {
+                    "$id": "/properties/public-transport-types/properties/train-junction-min-directions",
+                    "type": "integer"
+                }
+            },
+            "additionalProperties": False,
+            "required": ["train-junction-min-directions"]
 
-      }
-    },
-    "public-transport-ratings": {
-      "$id": "/properties/public-transport-ratings",
-      "type": "array",
-      "items": {
-        "$id": "/properties/public-transport-ratings/items",
-        "type": "object",
-        "properties": {
-          "min-seconds": {
-            "$id": "/properties/public-transport-ratings/items/properties/min-seconds",
-            "type": "integer"
-          },
-          "max-seconds": {
-            "$id": "/properties/public-transport-ratings/items/properties/max-seconds",
-            "type": "integer"
-          },
-          "transport-stop-categories": {
-            "$id": "/properties/public-transport-ratings/items/properties/transport-stop-categories",
+        },
+        "transport-stop-categories": {
+            "$id": "/properties/transport-stop-categories",
             "type": "array",
             "items": {
-              "$id": "/properties/public-transport-ratings/items/properties/transport-stop-categories/items",
-              "type": "object",
-              "properties": {
-                1: {
-                  "$ref": "#/definitions/pt-rating-category"
+                "$id": "/properties/transport-stop-categories/items",
+                "type": "object",
+                "properties": {
+                    "max-interval": {
+                        "$id": "/properties/transport-stop-categories/items/properties/max-interval",
+                        "$ref": "#/definitions/transport-stop-interval"
+                    },
+                    "min-interval": {
+                        "$id": "/properties/transport-stop-categories/items/properties/max-interval",
+                        "$ref": "#/definitions/transport-stop-interval"
+                    },
+                    "transport-type-mappings": {
+                        "$id": "/properties/transport-stop-categories/items/properties/transport-type-mappings",
+                        "type": "array",
+                        "items": {
+                            "$id": "/properties/transport-stop-categories/items/properties/transport-type-mappings/items",
+                            "type": "object",
+                            "properties": {
+                                "A": {
+                                    "$ref": "#/definitions/pt-stop-category"
+                                },
+                                "B": {
+                                    "$ref": "#/definitions/pt-stop-category"
+                                },
+                                "C": {
+                                    "$ref": "#/definitions/pt-stop-category"
+                                }
+                            },
+                            "additionalProperties": False,
+                        }
+                    }
                 },
-                2: {
-                  "$ref": "#/definitions/pt-rating-category"
-                },
-                3: {
-                  "$ref": "#/definitions/pt-rating-category"
-                },
-                4: {
-                  "$ref": "#/definitions/pt-rating-category"
-                },
-                5: {
-                  "$ref": "#/definitions/pt-rating-category"
-                },
-                6: {
-                  "$ref": "#/definitions/pt-rating-category"
-                },
-                7: {
-                  "$ref": "#/definitions/pt-rating-category"
-                }
-              },
-              "additionalProperties": False,
-            }
-          }
-        },
-        "additionalProperties": False,
-        "required": ["min-seconds", "max-seconds", "transport-stop-categories"]
+                "additionalProperties": False,
+                "required": ["transport-type-mappings"]
 
-      }
+            }
+        },
+        "public-transport-ratings": {
+            "$id": "/properties/public-transport-ratings",
+            "type": "array",
+            "items": {
+                "$id": "/properties/public-transport-ratings/items",
+                "type": "object",
+                "properties": {
+                    "min-seconds": {
+                        "$id": "/properties/public-transport-ratings/items/properties/min-seconds",
+                        "type": "integer"
+                    },
+                    "max-seconds": {
+                        "$id": "/properties/public-transport-ratings/items/properties/max-seconds",
+                        "type": "integer"
+                    },
+                    "transport-stop-categories": {
+                        "$id": "/properties/public-transport-ratings/items/properties/transport-stop-categories",
+                        "type": "array",
+                        "items": {
+                            "$id": "/properties/public-transport-ratings/items/properties/transport-stop-categories/items",
+                            "type": "object",
+                            "properties": {
+                                1: {
+                                    "$ref": "#/definitions/pt-rating-category"
+                                },
+                                2: {
+                                    "$ref": "#/definitions/pt-rating-category"
+                                },
+                                3: {
+                                    "$ref": "#/definitions/pt-rating-category"
+                                },
+                                4: {
+                                    "$ref": "#/definitions/pt-rating-category"
+                                },
+                                5: {
+                                    "$ref": "#/definitions/pt-rating-category"
+                                },
+                                6: {
+                                    "$ref": "#/definitions/pt-rating-category"
+                                },
+                                7: {
+                                    "$ref": "#/definitions/pt-rating-category"
+                                }
+                            },
+                            "additionalProperties": False,
+                        }
+                    }
+                },
+                "additionalProperties": False,
+                "required": ["min-seconds", "max-seconds", "transport-stop-categories"]
+
+            }
+        }
     }
-  }
 }
 
 
@@ -307,7 +307,7 @@ def load_config(config_path: str) -> dict:
     Loads the configuration and creates the default configuration if it does not yet exist.
     """
 
-    if not os.path.exists(config_path):
+    if not path.exists(config_path):
         create_default_config(config_path)
 
     with open(config_path, 'r') as f:
@@ -315,7 +315,7 @@ def load_config(config_path: str) -> dict:
 
     jsonschema.validate(config, SCHEMA, format_checker=jsonschema.FormatChecker())
 
-    return config
+    return _parse_dates(config)
 
 
 def create_default_config(config_path: str):
@@ -324,3 +324,11 @@ def create_default_config(config_path: str):
     """
     with open(config_path, 'w') as f:
         f.write(DEFAULT_CONFIG)
+
+
+def _parse_dates(config: dict) -> dict:
+    """Parse due dates as datetime objects"""
+    for due_date_config in config['due-dates']:
+        due_date_config['due-date'] = datetime.strptime(due_date_config['due-date'], "%Y-%m-%d")
+
+    return config
