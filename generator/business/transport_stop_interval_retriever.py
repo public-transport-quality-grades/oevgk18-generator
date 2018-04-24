6a397a9a0792cfe441ad78f9d161d5182cfceb9a
Robin Suter <suter.robin@gmail.com>
2018-04-24 09:55:15 +0200

    more robust implementation of interval retriever

--- generator/business/transport_stop_interval_retriever.py
+++ generator/business/transport_stop_interval_retriever.py
@@ -8,8 +8,8 @@ def get_transport_stop_interval(registry: dict,  due_date_config: dict, uic_ref:
     timetable_service = registry['timetable_service']
     db_config = registry['config']['database-connections']
     due_date: datetime = due_date_config['due-date']
-    start_time: datetime = _parse_time(due_date_config['lower-bound'])
-    end_time: datetime = _parse_time(due_date_config['upper-bound'])
+    start_time: datetime = _parse_time(due_date_config['lower-bound'], due_date)
+    end_time: datetime = _parse_time(due_date_config['upper-bound'], due_date)
 
     with timetable_service.db_connection(db_config) as db:
         all_departures: List[datetime] = timetable_service.get_departure_times(db, uic_ref, due_date)
@@ -23,17 +23,22 @@ def _calculate_transport_stop_interval(
     departures: List[datetime] = list(filter(
         lambda t: _departure_time_inside_interval(t, start_time, end_time), all_departures))
 
-    departures.sort()
+    if len(departures) == 1:
+        # if there is just one departure, duplicate it to use it as start and end
+        departures += departures
 
-    print(f"departures: {departures}")
+    departures.sort()
 
     interval_delta: timedelta = end_time - start_time
 
-    first_scheduled_departure_after_interval: datetime = min(filter(lambda t: t > end_time, all_departures))
-    # TODO: Handle if end_time is midnight, there are no departures after that
     first_fictional_departure_after_interval: datetime = departures[0] + interval_delta
 
-    end_departure = min(first_scheduled_departure_after_interval, first_fictional_departure_after_interval)
+    departures_after_interval = list(filter(lambda t: t > end_time, all_departures))
+    if departures_after_interval:
+        first_scheduled_departure_after_interval: datetime = min(departures_after_interval)
+        end_departure = min(first_scheduled_departure_after_interval, first_fictional_departure_after_interval)
+    else:
+        end_departure = first_fictional_departure_after_interval
 
     waiting_delta_sum = sum([
         _calculate_waiting_delta(i, departures, end_departure, start_time, end_time)
@@ -53,8 +58,6 @@ def _parse_time(time_str: str, due_date: datetime) -> datetime:
 
 
 def _departure_time_inside_interval(t: datetime, start_time: datetime, end_time: datetime) -> bool:
-    if not start_time <= t < end_time:
-        print(f"{t} is not in interval")
     return start_time <= t < end_time
 
 
