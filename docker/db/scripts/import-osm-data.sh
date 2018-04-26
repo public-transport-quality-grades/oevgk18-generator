2e9e99341f01538c5ca2a5632b04313d242bdef4
Robin Suter <suter.robin@gmail.com>
2018-04-26 21:10:05 +0200

    remove import and dependecies of pt_stop table

--- docker/db/scripts/import-osm-data.sh
+++ docker/db/scripts/import-osm-data.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
 ./scripts/osm-scripts/import-routing-data.sh
-./scripts/osm-scripts/import-pt-stop-data.sh
 
 cat /post-import-sql/osm-import/* | psql -U "$POSTGRES_USER" -q "$POSTGRES_DB" -h db -f -
