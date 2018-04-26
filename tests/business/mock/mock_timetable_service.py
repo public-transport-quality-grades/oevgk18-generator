1f1daaa54ee465f3330d3df61b15b889e7f49632
Robin Suter <suter.robin@gmail.com>
2018-04-26 09:59:27 +0200

    mock timetable_service to avoid breaking method encapsulation

--- tests/business/mock/mock_timetable_service.py
+++ tests/business/mock/mock_timetable_service.py
@@ -1,11 +1,45 @@
+from contextlib import contextmanager
+from typing import List
+from datetime import datetime
 
-
-mocked_return_values = {
+mocked_directions_values = {
     '8503400': 9,
     '8503125': 5,
     '8591382': 0
 }
 
+mocked_departure_times = {
+    '8503400': [datetime(2018, 4, 23, 6, 35), datetime(2018, 4, 23, 7, 15)],
+
+    '8503125': [datetime(2018, 4, 23, 9, 0), datetime(2018, 4, 23, 9, 15), datetime(2018, 4, 23, 9, 30),
+                datetime(2018, 4, 23, 9, 45), datetime(2018, 4, 23, 10, 0), datetime(2018, 4, 23, 10, 15),
+                datetime(2018, 4, 23, 10, 30), datetime(2018, 4, 23, 10, 45), datetime(2018, 4, 23, 11, 0),
+                datetime(2018, 4, 23, 11, 15), datetime(2018, 4, 23, 11, 30), datetime(2018, 4, 23, 11, 45),
+                datetime(2018, 4, 23, 12, 0), datetime(2018, 4, 23, 12, 15), datetime(2018, 4, 23, 12, 30),
+                datetime(2018, 4, 23, 12, 45), datetime(2018, 4, 23, 13, 0), datetime(2018, 4, 23, 13, 15),
+                datetime(2018, 4, 23, 13, 30), datetime(2018, 4, 23, 13, 45), datetime(2018, 4, 23, 14, 0),
+                datetime(2018, 4, 23, 14, 15), datetime(2018, 4, 23, 14, 30), datetime(2018, 4, 23, 14, 45),
+                datetime(2018, 4, 23, 15, 0), datetime(2018, 4, 23, 15, 15), datetime(2018, 4, 23, 15, 30),
+                datetime(2018, 4, 23, 15, 45), datetime(2018, 4, 23, 16, 0), datetime(2018, 4, 23, 16, 15),
+                datetime(2018, 4, 23, 16, 30), datetime(2018, 4, 23, 16, 45)],
+
+    '8591382': [datetime(2018, 4, 23, 9, 1), datetime(2018, 4, 23, 9, 2), datetime(2018, 4, 23, 10, 1),
+                datetime(2018, 4, 23, 10, 2), datetime(2018, 4, 23, 11, 0), datetime(2018, 4, 23, 11, 5)]
+}
+
+
+@contextmanager
+def db_connection(db_config: dict):
+    yield None
+
 
 def get_count_of_distinct_next_stops(db_config: dict, uic_ref: str) -> int:
-    return mocked_return_values[uic_ref]
+    return mocked_directions_values[uic_ref]
+
+
+def get_departure_times(db, uic_ref: str, due_date: datetime) -> List[datetime]:
+    return mocked_departure_times[uic_ref]
+
+
+def prepare_calendar_table(db, due_date: datetime):
+    pass
--- tests/business/test_transport_stop_interval_retriever.py
+++ tests/business/test_transport_stop_interval_retriever.py
@@ -1,59 +1,74 @@
 from datetime import datetime
 from ..context import generator
+from .mock import mock_timetable_service
 
 
 def test_calculate_transport_stop_interval_trivial():
     """example from the VISUM 12 book"""
 
-    departures = [datetime(2018, 4, 23, 6, 35), datetime(2018, 4, 23, 7, 15)]
+    uic_refs = ['8503400']
 
-    start_time = datetime(2018, 4, 23, 6, 0)
-    end_time = datetime(2018, 4, 23, 7, 0)
+    due_date_config = {
+        'due-date': datetime(2018, 4, 23),
+        'lower-bound': '06:00',
+        'upper-bound': '07:00'
+    }
 
-    expected_interval = 2600
+    expected_interval = {
+        '8503400': 2600
+    }
 
-    result = generator.business.transport_stop_interval_retriever._calculate_transport_stop_interval(
-        departures, start_time, end_time)
+    result = generator.business.transport_stop_interval_retriever.get_transport_stop_intervals(
+        _mock_registry(), due_date_config, uic_refs)
 
     assert result == expected_interval
 
 
 def test_calculate_transport_stop_interval_regular():
-    departures = [datetime(2018, 4, 23, 9, 0), datetime(2018, 4, 23, 9, 15), datetime(2018, 4, 23, 9, 30),
-                  datetime(2018, 4, 23, 9, 45), datetime(2018, 4, 23, 10, 0), datetime(2018, 4, 23, 10, 15),
-                  datetime(2018, 4, 23, 10, 30), datetime(2018, 4, 23, 10, 45), datetime(2018, 4, 23, 11, 0),
-                  datetime(2018, 4, 23, 11, 15), datetime(2018, 4, 23, 11, 30), datetime(2018, 4, 23, 11, 45),
-                  datetime(2018, 4, 23, 12, 0), datetime(2018, 4, 23, 12, 15), datetime(2018, 4, 23, 12, 30),
-                  datetime(2018, 4, 23, 12, 45), datetime(2018, 4, 23, 13, 0), datetime(2018, 4, 23, 13, 15),
-                  datetime(2018, 4, 23, 13, 30), datetime(2018, 4, 23, 13, 45), datetime(2018, 4, 23, 14, 0),
-                  datetime(2018, 4, 23, 14, 15), datetime(2018, 4, 23, 14, 30), datetime(2018, 4, 23, 14, 45),
-                  datetime(2018, 4, 23, 15, 0), datetime(2018, 4, 23, 15, 15), datetime(2018, 4, 23, 15, 30),
-                  datetime(2018, 4, 23, 15, 45), datetime(2018, 4, 23, 16, 0), datetime(2018, 4, 23, 16, 15),
-                  datetime(2018, 4, 23, 16, 30), datetime(2018, 4, 23, 16, 45)]
-
     # with more data, the algorithm gets closer to the "real" interval
 
-    start_time = datetime(2018, 4, 23, 8, 40)
-    end_time = datetime(2018, 4, 23, 16, 40)
+    uic_refs = ['8503125']
 
-    expected_interval = 890.625  # 14 min 50s
+    due_date_config = {
+        'due-date': datetime(2018, 4, 23),
+        'lower-bound': '08:40',
+        'upper-bound': '16:40'
+    }
 
-    result = generator.business.transport_stop_interval_retriever._calculate_transport_stop_interval(
-        departures, start_time, end_time)
+    expected_interval = {
+        '8503125': 890.625  # 14 min 50s
+    }
+
+    result = generator.business.transport_stop_interval_retriever.get_transport_stop_intervals(
+        _mock_registry(), due_date_config, uic_refs)
 
     assert result == expected_interval
 
 
 def test_calculate_transport_stop_interval_skewed():
-    departures = [datetime(2018, 4, 23, 9, 1), datetime(2018, 4, 23, 9, 2), datetime(2018, 4, 23, 10, 1),
-                  datetime(2018, 4, 23, 10, 2), datetime(2018, 4, 23, 11, 0), datetime(2018, 4, 23, 11, 5)]
 
-    start_time = datetime(2018, 4, 23, 9, 0)
-    end_time = datetime(2018, 4, 23, 11, 0)
+    uic_refs = ['8591382']
 
-    expected_interval = 3481.5  # 58.01 min
+    due_date_config = {
+        'due-date': datetime(2018, 4, 23),
+        'lower-bound': '09:00',
+        'upper-bound': '11:00'
+    }
 
-    result = generator.business.transport_stop_interval_retriever._calculate_transport_stop_interval(
-        departures, start_time, end_time)
+    expected_interval = {
+        '8591382': 3481.5  # 58.01 min
+    }
+
+    result = generator.business.transport_stop_interval_retriever.get_transport_stop_intervals(
+        _mock_registry(), due_date_config, uic_refs)
 
     assert result == expected_interval
+
+
+def _mock_registry():
+    return {
+        'timetable_service': mock_timetable_service,
+        'config': {
+            'database-connections': {},
+        }
+    }
