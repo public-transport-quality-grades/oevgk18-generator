8bc3ed140e0311216a2500f0e6abee0ad1065ff4
Jonas Matter <jonas.matter@bluewin.ch>
2018-05-04 15:53:55 +0200

    rename get_source to get_nearest_neighbour

--- docker/db/function/105_create_get_nearest_neighbour_function.sql
+++ docker/db/function/105_create_get_nearest_neighbour_function.sql
@@ -1,5 +1,11 @@
-CREATE OR REPLACE FUNCTION get_source(stop_geom GEOMETRY) RETURNS INTEGER AS $$
-   SELECT id FROM vertex
-	      ORDER BY geom_vertex <#> stop_geom
-	      LIMIT 1;
-$$ LANGUAGE sql;
+CREATE OR REPLACE FUNCTION get_nearest_neighbour(stop_geom GEOMETRY) RETURNS RECORD AS $$
+DECLARE
+  ret RECORD;
+BEGIN
+  SELECT id, St_DistanceSphere(geom_vertex, stop_geom)
+  FROM vertex
+    ORDER BY geom_vertex <#> stop_geom
+    LIMIT 1 into ret;
+   RETURN ret;
+END;
+$$ LANGUAGE plpgsql;
