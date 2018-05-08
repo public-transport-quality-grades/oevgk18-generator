20b745d169d8921c0f70f32b3dc77ef5d0beb631
Robin Suter <suter.robin@gmail.com>
2018-05-08 14:37:48 +0200

    remove unused module

--- generator/business/transport_stop_resolver.py
+++
@@ -1,7 +0,0 @@
-from typing import List
-
-
-def transport_stops(registry) -> List[str]:
-    ptstop_service = registry['ptstop_service']
-    db_config = registry['config']['database-connections']
-    return list(ptstop_service.get_transport_stops(db_config))
