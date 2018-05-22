219b3e87d6d74976cebf08ee2452bf7c65d623dd
Robin Suter <suter.robin@gmail.com>
2018-05-22 09:34:36 +0200

    optimize get_isochrones function with edge preselection

--- docker/db/function/107_create_isochrones_function.sql
+++ docker/db/function/107_create_isochrones_function.sql
@@ -1,11 +1,21 @@
 CREATE OR REPLACE FUNCTION isochrones(start_vertex INTEGER, boundaries NUMERIC[])
   RETURNS TABLE(distance NUMERIC, polygon GEOMETRY) AS $$
+DECLARE
+  start_vertex_geom GEOMETRY;
 BEGIN
+  SELECT the_geom INTO start_vertex_geom FROM routing_segmented_vertices_pgr WHERE id = start_vertex;
+
+  CREATE TEMP TABLE edge_preselection ON COMMIT DROP AS (
+    SELECT id, source, target, cost_effective AS cost FROM routing_segmented r
+      -- select edges within ~1200 meters from the start vertex
+    WHERE ST_intersects(ST_Buffer(start_vertex_geom, 0.013), r.geom_way)
+  );
+
   CREATE TEMP TABLE distances ON COMMIT DROP AS (
 	  SELECT id, boundary AS distance, the_geom as point
 			FROM unnest(boundaries) boundary,
 			LATERAL pgr_drivingDistance(
-          'SELECT id, source, target, cost_effective AS cost FROM routing_segmented',
+          'SELECT id, source, target, cost FROM edge_preselection',
           start_vertex,
           boundary,
           FALSE
