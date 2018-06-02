7df23ea7b1a88fc5c9923566af016efef4d695fe
Robin Suter <suter.robin@gmail.com>
2018-06-02 14:09:11 +0200

    fix transport_stop mock

--- tests/business/mock/mock_transport_stops.py
+++ tests/business/mock/mock_transport_stops.py
@@ -4,5 +4,5 @@ stop_8503400 = TransportStop('', 8503400, False, None, [RouteType.RAIL.value, Ro
 stop_8503125 = TransportStop('', 8503125, False, None, [RouteType.RAIL.value, RouteType.SUBWAY.value])
 stop_8591382 = TransportStop('', 8591382, False, None, [RouteType.BUS.value])
 stop_8593245 = TransportStop('', 8593245, False, None, [RouteType.RAIL.value])
-stop_8504532 = TransportStop('', 8504532, True, None, [RouteType.RAIL.value]),
+stop_8504532 = TransportStop('', 8504532, True, None, [RouteType.RAIL.value])
 
