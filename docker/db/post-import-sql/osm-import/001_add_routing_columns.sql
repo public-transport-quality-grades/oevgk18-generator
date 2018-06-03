1d0ba89fbe069debe9b7b1ae7fcb1bf7151a9f48
Robin Suter <suter.robin@gmail.com>
2018-06-03 15:01:59 +0200

    remove unneeded column

--- docker/db/post-import-sql/osm-import/001_add_routing_columns.sql
+++ docker/db/post-import-sql/osm-import/001_add_routing_columns.sql
@@ -1,5 +1,3 @@
 -- routing table will be overridden with a different schema during the import
 ALTER TABLE routing
-  ADD COLUMN IF NOT EXISTS relevant bool;
-ALTER TABLE routing
-  ADD COLUMN IF NOT EXISTS cost_effective DOUBLE PRECISION;
+  ADD COLUMN IF NOT EXISTS relevant BOOLEAN;
