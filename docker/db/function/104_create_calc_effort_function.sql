bcb72c1bda988749989c60677eda54389c4e37b7
Jonas Matter <jonas.matter@bluewin.ch>
2018-06-05 16:05:49 +0200

    use correct unit and move segment length to variable in calc effort

--- docker/db/function/104_create_calc_effort_function.sql
+++ docker/db/function/104_create_calc_effort_function.sql
@@ -2,6 +2,7 @@ CREATE OR REPLACE FUNCTION get_effort(gid_input INTEGER)
   RETURNS DOUBLE PRECISION AS $$
 DECLARE
   segment                    RECORD;
+  segment_length             INTEGER := 10;
   distance                   DOUBLE PRECISION;
   incline                    INTEGER := 0;
   decline                    INTEGER := 0;
@@ -14,16 +15,16 @@ BEGIN
     get_height(transform_to_2056(st_startpoint(seg.geom))) AS start,
     get_height(transform_to_2056(st_endpoint(seg.geom)))   AS destination
   FROM routing_segmented AS r
-    CROSS JOIN segments(r.geom_way, r.cost, 10) AS seg
+    CROSS JOIN segments(r.geom_way, r.cost, segment_length) AS seg
   WHERE r.id = gid_input
   LOOP
     IF segment.start < segment.destination
     THEN
-      incline := incline + 1;
+      incline := incline + segment_length;
       incline_metres_in_altitude := incline_metres_in_altitude + (segment.destination - segment.start);
     ELSEIF segment.start > segment.destination
       THEN
-        decline := decline + 1;
+        decline := decline + segment_length;
         decline_metres_in_altitude := decline_metres_in_altitude + segment.start - segment.destination;
     END IF;
   END LOOP;
@@ -33,10 +34,10 @@ BEGIN
   FROM routing_segmented
   WHERE id = gid_input;
 
-  effort := distance + incline_metres_in_altitude / 100;
+  effort := distance + (incline_metres_in_altitude / 100) * 1000;
   IF decline != 0 AND decline_metres_in_altitude / decline > 0.2
   THEN
-    effort := effort + decline_metres_in_altitude / 150;
+    effort := effort + (decline_metres_in_altitude / 150) * 1000;
   END IF;
   RETURN effort;
 END;
